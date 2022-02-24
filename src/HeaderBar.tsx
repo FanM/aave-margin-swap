fb7cb0bdd1bbf46ca596a092d6b49e77162d7300
maofan <maofan1983@gmail.com>
2022-02-24 21:13:44 +0800

    Added logo icon

--- src/HeaderBar.tsx
+++ src/HeaderBar.tsx
@@ -5,11 +5,11 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import ExchangeIcon from "@mui/icons-material/CurrencyExchange";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert, { AlertProps } from "@mui/material/Alert";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { lightBlue, purple } from "@mui/material/colors";
+import { createSvgIcon } from "@mui/material/utils";
 
 import {
   Web3ReactProvider,
@@ -26,6 +26,11 @@ import { Web3Provider } from "@ethersproject/providers";
 import { useEagerConnect, useInactiveListener } from "./hooks";
 import WalletMenu from "./WalletMenu";
 import AssetPanel from "./AssetPanel";
+
+const LogoIcon = createSvgIcon(
+  <path d="M19.375 15.103A8.001 8.001 0 0 0 8.03 5.053l-.992-1.737A9.996 9.996 0 0 1 17 3.34c4.49 2.592 6.21 8.142 4.117 12.77l1.342.774l-4.165 2.214l-.165-4.714l1.246.719zM4.625 8.897a8.001 8.001 0 0 0 11.345 10.05l.992 1.737A9.996 9.996 0 0 1 7 20.66C2.51 18.068.79 12.518 2.883 7.89L1.54 7.117l4.165-2.214l.165 4.714l-1.246-.719zm8.79 5.931L10.584 12l-2.828 2.828l-1.414-1.414l4.243-4.242L13.414 12l2.829-2.828l1.414 1.414l-4.243 4.242z" />,
+  "Home"
+);
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
   props,
@@ -107,7 +112,7 @@ const AppToolBar = () => {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <ExchangeIcon />
+            <LogoIcon sx={{ fontSize: 40 }} />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             AAVE LEVERAGED SWAP
